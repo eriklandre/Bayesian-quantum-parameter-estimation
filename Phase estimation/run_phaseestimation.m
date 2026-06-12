2f3d2416532c69ed006ab0b01a983c4fb6c521a0
Erik Luszczak André <eluszcan10@alumnes.ub.edu>
2026-06-12 10:54:40 +0200

    Add files via upload

---
+++ Phase estimation/run_phaseestimation.m
@@ -0,0 +1,15 @@
+tic
+method = 3;
+ncopies = 2;
+strategy = 1;
+no = 20;
+p_vals = linspace(0,1,21);
+scores = zeros(length(p_vals), 1);
+%scores_adaptive = zeros(length(No_values), 1);
+for i = 1:length(p_vals)
+    p = p_vals(i);
+    [~,score] = script_concatenation_kcopy(no, no, method, ncopies, strategy, p);
+    scores(i) = score;
+end
+disp(scores);
+toc
