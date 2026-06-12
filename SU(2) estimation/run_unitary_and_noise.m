01c0c77ef15b8c3b85da3bfeb648b678fff7890b
Erik Luszczak André <eluszcan10@alumnes.ub.edu>
2026-06-12 12:04:20 +0200

    Update run_unitary_and_noise.m

--- SU(2) estimation/run_unitary_and_noise.m
+++ SU(2) estimation/run_unitary_and_noise.m
@@ -9,7 +9,7 @@ p_values = linspace(0,1,21);
 score = zeros(length(p_values),1);
 for idx = 1:length(p_values)
     p = p_values(idx);
-    [~,score(idx)] = unitary_and_noise_kcopy(method, ncopies, strategy, p);
+    [~,score(idx)] = unitary_and_noise_kcopy(ncopies, strategy, p);
     %score(idx) = unitary_and_noise_greedy(10, ncopies, n_monte_carlo, p);
     %score(idx) = unitary_and_noise_nonadaptive(10, ncopies, n_monte_carlo, p);
 end
